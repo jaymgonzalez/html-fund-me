ee23cc6e584cd51b48d33dd8a5e7bbd20b0b3913
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-15 13:07:45 +0100

    add signer

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
+import { ethers } from './ethers.js'
+
+const connectButton = document.getElementById('connectButton')
+const fundButton = document.getElementById('fundButton')
+
+connectButton.onclick = connect
+fundButton.onclick = fund
+
 async function connect() {
   if (typeof window.ethereum !== 'undefined') {
     await window.ethereum.request({ method: 'eth_requestAccounts' })
     console.log('Connected!')
-    document.getElementById('connectButton').innerHTML = 'Connected!'
+    connectButton.innerHTML = 'Connected!'
   } else {
-    document.getElementById('connectButton').innerHTML =
-      'Please install metamask'
+    connectButton.innerHTML = 'Please install metamask'
   }
 }
 
 async function fund(ethAmount) {
-  console.log(`Funding: ${ethAmount}`)
+  if (typeof window.ethereum !== 'undefined') {
+    console.log(`Funding: ${ethAmount}`)
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    console.log(signer)
+    // const contract
+  }
 }
