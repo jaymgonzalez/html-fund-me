438503fdb3433d10e64a31f7b22c733724a9d000
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-15 12:41:32 +0100

    refactor index

---
+++ index.js
@@ -0,0 +1,10 @@
+async function connect() {
+  if (typeof window.ethereum !== 'undefined') {
+    await window.ethereum.request({ method: 'eth_requestAccounts' })
+    console.log('Connected!')
+    document.getElementById('connectButton').innerHTML = 'Connected!'
+  } else {
+    document.getElementById('connectButton').innerHTML =
+      'Please install metamask'
+  }
+}
